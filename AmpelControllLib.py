83b4c8ca19ef042dec22be27f2e3441b87814d71
Felix <mail@nicidienase.de>
2013-06-06 00:19:26 +0200

    started a Lib for more complex Server Stuff

---
+++ AmpelControllLib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+## Ampel-Controll lib
+
+class AmpelControll:
+	def __init__(self):
+		pass
+
+# TODO: parse json with blink-settings
+# TODO: method to blink (params: color, on-duration, off-duration)
+# TODO: method for sequence of on-offs (params: color, interval, list of on/off)
