9571827d16cdac67e3e500b4b842522e28933583
Felix <mail@nicidienase.de>
2013-06-15 02:48:12 +0200

    ampel_server now with threads

--- ampel_server.py
+++ ampel_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 
 import socket
+import threading
+import time
 
 red = 24
 green = 23
@@ -9,6 +11,8 @@ def main():
 	init_pin(green)
 	init_pin(red)
 	s = socket.socket()
+	threadRed = None
+	threadGreen = None
 	try:
 #		host = socket.gethostname()
 		host = "0.0.0.0"
@@ -23,40 +27,57 @@ def main():
 				c.send("ohai\n")
 				c.send("Commands are '[red|green] [0|1]'\n")
 				while True: 
-					msg = c.recv(16);
+					msg = c.recv(32);
 					if len(msg) <= 2:
 						break
+					msg=msg[0:-2]
 					msg = msg.split(" ")
-					msg[1] = msg[1][0]
+#					msg[1] = msg[1][0]
 					print msg
 					if msg[0] == "green":
-						if msg[1] == '0':
-							c.send("Turning of green\n")
-							set_pin(green,0)
-						elif msg[1] == '1':
-							c.send("Turning on green\n")
-							set_pin(green,1)
-						else:
-							c.send("error\n")
+						if threadGreen != None:
+							threadGreen.stop()
+						if msg[1] != '0':
+							threadGreen = myBlinkThread(green,float(msg[1]),float(msg[2]))
+							threadGreen.start()
 					elif msg[0] == "red":
-						if msg[1] == '0':
-							c.send("Turning of red\n")
-							set_pin(red,0)
-						elif msg[1] == '1':
-							c.send("Turning on red\n")
-							set_pin(red,1)
-						else:
-							c.send("error\n")
+						if threadRed != None:
+							threadRed.stop()
+						if msg[1] != '0':
+							threadRed = myBlinkThread(red,float(msg[1]),float(msg[2]))
+							threadRed.start()
+					elif msg[0] == "both":
+						if threadRed != None:
+							threadRed.stop()
+						if threadGreen != None:
+							threadGreen.stop()
+						if msg[1] != '0':
+							threadRed = myBlinkThread(red,float(msg[1]),float(msg[2]))
+							threadGreen = myBlinkThread(green,float(msg[3]),float(msg[4]))
+							threadRed.start()
+							if len(msg) > 5:
+								time.sleep(float(msg[5]))
+							threadGreen.start()
 					else:
 						c.send("error\n")
 			except socket.error:
 				print "Connection reset"
-			finally:
+			except KeyboardInterrupt:
 				c.close()
+				if threadGreen != None:
+					threadGreen.stop()
+				if threadRed != None:
+					threadRed.stop()
 	except KeyboardInterrupt:
 		print "Sigterm\n"
+	except:
+		print "error\n"
 	finally:
 		print "kthxbye\n"
+		if threadGreen != None:
+			threadGreen.stop()
+		if threadRed != None:
+			threadRed.stop()
 		pin_off(red)
 		pin_off(green)
 #		s.shutdown(socket.SHUT_RDWR)
@@ -71,6 +92,24 @@ def init_pin(pin):
 
 def pin_off(pin):
 	file('/sys/class/gpio/unexport','w').write(str(pin))
+
+class myBlinkThread (threading.Thread):
+	"""docstring for myBlinkThread"""
+	def __init__(self, pin,ontime,offtime):
+		threading.Thread.__init__(self)
+		self.pin = pin
+		self.ontime = ontime
+		self.offtime = offtime
+		self.runFlag = True
+	def run(self):
+		while self.runFlag:
+			set_pin(self.pin,1)
+			time.sleep(self.ontime)
+			set_pin(self.pin,0)
+			time.sleep(self.offtime)
+	def stop(self):
+		self.runFlag = False
+
 
 if __name__ == "__main__":
 	main()
