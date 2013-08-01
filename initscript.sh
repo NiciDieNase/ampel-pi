74169b757259a61a4d8f3e8a4a8c04af375e5b91
Felix <mail@nicidienase.de>
2013-08-01 22:23:06 +0200

    added simple initscript

---
+++ initscript.sh
@@ -0,0 +1,23 @@
+! /bin/sh
+# /etc/init.d/ampel
+#
+
+# Some things that run always
+touch /var/lock/ampel
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+	start)
+		/home/pi/ampel/ampelwebserver.py >> /var/log/ampel.log 2>> /var/log/ampel.log &
+		;;
+	stop)
+		echo "Stopping script blah"
+		echo "Could do more here"
+		;;
+	*)
+		echo "Usage: /etc/init.d/blah {start|stop}"
+		exit 1
+		;;
+esac
+
+exit 0
